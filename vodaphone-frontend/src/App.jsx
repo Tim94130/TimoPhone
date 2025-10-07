bb4a013668bea4abbc5e990852c91a9fd14082d8
Tim94130 <timothe.billard.sony@gmail.com>
2025-10-07 23:55:52 +0200

    Amélioration du design de la nav 2

--- vodaphone-frontend/src/App.jsx
+++ vodaphone-frontend/src/App.jsx
@@ -5,7 +5,7 @@ import Register from "./pages/Register";
 import Login from "./pages/Login";
 import Profile from "./pages/Profile";
 import ChangePassword from "./pages/ChangePassword";
-import "./Nav.css"; // ✅ on importe le fichier CSS
+import "./nav.css"; // ✅ on importe le fichier CSS
 
 function Nav() {
   const { isLoggedIn, logout } = useContext(AuthContext);
