14eefb255331711c9dac4d0439913446618919ff
Tim94130 <timothe.billard.sony@gmail.com>
2025-10-07 19:01:06 +0200

    front a jour

--- vodaphone-frontend/src/api.js
+++ vodaphone-frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000", // backend
+  baseURL: "https://timophone.onrender.com", // ton back Render
 });
 
 export default api;
