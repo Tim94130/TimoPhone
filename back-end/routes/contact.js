216269d40a317da09bfea7f3c8e90273390319d2
Tim94130 <timothe.billard.sony@gmail.com>
2025-10-07 19:31:15 +0200

    test

--- back-end/routes/contact.js
+++ back-end/routes/contact.js
@@ -4,12 +4,6 @@ const router = express.Router();
 const requireAuth = require("../middleware/requireAuth");
 const contactController = require("../controllers/contactController");
 
-router.use(cors({
-  origin: "https://68e54249f4b898654b3710e4--vodaphone.netlify.app",
-  methods: ["GET", "POST", "PATCH", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-}));
-
 router.use(requireAuth);
 
 router.get("/", contactController.getContacts);    
